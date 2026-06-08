7df81ddea001e8992e9681338ade78171e9b5a0a
waimea-bsjenner <bsjenner@waimea.school.nz>
2026-06-08 12:26:32 +1200

    tryna learn how to upload images

--- app/__init__.py
+++ app/__init__.py
@@ -10,6 +10,11 @@ from os import getenv
 from io import BytesIO
 import html
 from app.helpers import *
+import os
+import uuid
+from werkzeug.utils import secure_filename
+
+UPLOAD_FOLDER = os.path.join('app', 'static', 'uploads')
 
 
 # Create the app
@@ -35,7 +40,7 @@ def show_welcome():
 def show_all_creatures():
     with connect_db() as db:
         sql = """
-            SELECT id, species, name
+            SELECT id, species, name, image_file
             FROM creatures
         """
         params = ()
@@ -43,6 +48,42 @@ def show_all_creatures():
 
         return render_template("pages/creature_list.jinja", creatures=creatures)
 
+#-----------------------------------------------------------
+# New Creature Form
+#-----------------------------------------------------------
+@app.get("/creature/new")
+def get_new_creature():
+    return render_template("pages/creature_new_form.jinja")
+
+#-----------------------------------------------------------
+# Route to add a new creature
+#-----------------------------------------------------------
+@app.post("/add_creature")
+def add_creature():
+    name = request.form.get('name','').strip()
+    name = html.escape(name)
+
+    species = request.form.get('species','').strip()
+    species = html.escape(species)
+
+    image = request.files.get('image', None)
+    if not image or image.filename == '':
+        flash("There was a problem uploading the image...", "error")
+        return redirect("pages/creature_new_form.jinja")
+    
+    filename = secure_filename(image.filename)
+    random_prefix = uuid.uuid4().hex[:12]
+    unique_filename = f"{random_prefix}_{filename}"
+
+    filepath = os.path.join(UPLOAD_FOLDER, unique_filename)
+
+    image.save(filename)
+
+    with connect_db() as db:
+        sql = "INSERT INTO creatures (name, species, image.filename) VALUES (?, ?, ?)"
+        params = (name, species, unique_filename)
+        db.execute(sql, params)
+        return redirect()
 
 #-----------------------------------------------------------
 # Help page - Show some help
